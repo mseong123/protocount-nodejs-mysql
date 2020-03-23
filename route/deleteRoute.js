8f77212f8fa8437bc8fb1de13dcadefa572e70e9
mseong <mseong123@hotmail.com>
2020-03-23 16:30:53 +0800

    added feature delete multiple items in process page for each item - change to both front end and mysql db stored procedures

--- route/deleteRoute.js
+++ route/deleteRoute.js
@@ -1,104 +1,105 @@
-function deleteAll(req,res,next,pool) {
-    pool.query('DELETE FROM '+pool.escapeId(req.body.item)+' WHERE '+pool.escapeId(req.body.item+'_NUM')+'=?',[req.body.id],(error,data,field)=>{
-        res.send({error,data,field});
-    })
-}
-
-
 function deleteRoute(req,res,next,pool) {
     if (req.body)
     switch (req.body.item) {
         case 'stock':
-        deleteAll(req,res,next,pool);
+        pool.query('CALL DELETE_STOCK(?)',[req.body.id.map(item=>JSON.stringify(item)).join()],(error,data,field)=>{
+            res.send({error,data,field});
+        })
         break;
 
         case 'debtor':
-        deleteAll(req,res,next,pool);
+        pool.query('CALL DELETE_DEBTOR(?)',[req.body.id.map(item=>JSON.stringify(item)).join()],(error,data,field)=>{
+            res.send({error,data,field});
+        })
         break; 
 
         case 'creditor':
-        deleteAll(req,res,next,pool);
+        pool.query('CALL DELETE_CREDITOR(?)',[req.body.id.map(item=>JSON.stringify(item)).join()],(error,data,field)=>{
+            res.send({error,data,field});
+        })
         break; 
 
         case 'delivery_order':
-        pool.query('CALL DELETE_DELIVERY_ORDER(?)',[req.body.id],(error,data,field)=>{
-            res.send({error,data:data?data[0]:null,field:field?field[0]:null});
+        pool.query('CALL DELETE_DELIVERY_ORDER(?)',[req.body.id.map(item=>JSON.stringify(item)).join()],(error,data,field)=>{
+            res.send({error,data,field});
         })
         break; 
 
         case 'sales_invoice':
-        pool.query('CALL DELETE_SALES_INVOICE(?)',[req.body.id],(error,data,field)=>{
-            res.send({error,data:data?data[0]:null,field:field?field[0]:null});
+        pool.query('CALL DELETE_SALES_INVOICE(?)',[req.body.id.map(item=>JSON.stringify(item)).join()],(error,data,field)=>{
+            res.send({error,data,field});
         })
         break; 
 
         case 'debit_note':
-        pool.query('CALL DELETE_DEBIT_NOTE(?)',[req.body.id],(error,data,field)=>{
-            res.send({error,data:data?data[0]:null,field:field?field[0]:null});
+        pool.query('CALL DELETE_DEBIT_NOTE(?)',[req.body.id.map(item=>JSON.stringify(item)).join()],(error,data,field)=>{
+            res.send({error,data,field});
         })
         break; 
 
         case 'credit_note':
-        pool.query('CALL DELETE_CREDIT_NOTE(?)',[req.body.id],(error,data,field)=>{
-            res.send({error,data:data?data[0]:null,field:field?field[0]:null});
+        pool.query('CALL DELETE_CREDIT_NOTE(?)',[req.body.id.map(item=>JSON.stringify(item)).join()],(error,data,field)=>{
+            res.send({error,data,field});
         })
         break; 
 
         case 'purchase_return':
-        pool.query('CALL DELETE_PURCHASE_RETURN(?)',[req.body.id],(error,data,field)=>{
-            res.send({error,data:data?data[0]:null,field:field?field[0]:null});
+        pool.query('CALL DELETE_PURCHASE_RETURN(?)',[req.body.id.map(item=>JSON.stringify(item)).join()],(error,data,field)=>{
+            res.send({error,data,field});
         })
         break; 
 
         case 'purchase_invoice':
-        pool.query('CALL DELETE_PURCHASE_INVOICE(?)',[req.body.id],(error,data,field)=>{
-            res.send({error,data:data?data[0]:null,field:field?field[0]:null});
+        pool.query('CALL DELETE_PURCHASE_INVOICE(?)',[req.body.id.map(item=>JSON.stringify(item)).join()],(error,data,field)=>{
+            res.send({error,data,field});
         })
         break; 
 
         case 'purchase_debit_note':
-        pool.query('CALL DELETE_PURCHASE_DEBIT_NOTE(?)',[req.body.id],(error,data,field)=>{
-            res.send({error,data:data?data[0]:null,field:field?field[0]:null});
+        pool.query('CALL DELETE_PURCHASE_DEBIT_NOTE(?)',[req.body.id.map(item=>JSON.stringify(item)).join()],(error,data,field)=>{
+            res.send({error,data,field});
         })
         break; 
 
         case 'purchase_credit_note':
-        pool.query('CALL DELETE_PURCHASE_CREDIT_NOTE(?)',[req.body.id],(error,data,field)=>{
-            res.send({error,data:data?data[0]:null,field:field?field[0]:null});
+        pool.query('CALL DELETE_PURCHASE_CREDIT_NOTE(?)',[req.body.id.map(item=>JSON.stringify(item)).join()],(error,data,field)=>{
+            res.send({error,data,field});
         })
         break; 
 
         case 'bank':
-        deleteAll(req,res,next,pool);
+        pool.query('CALL DELETE_BANK(?)',[req.body.id.map(item=>JSON.stringify(item)).join()],(error,data,field)=>{
+            res.send({error,data,field});
+        })
         break; 
 
         case 'bank_receipt':
-        pool.query('CALL DELETE_BANK_RECEIPT(?)',[req.body.id],(error,data,field)=>{
-            res.send({error,data:data?data[0]:null,field:field?field[0]:null});
+        pool.query('CALL DELETE_BANK_RECEIPT(?)',[req.body.id.map(item=>JSON.stringify(item)).join()],(error,data,field)=>{
+            res.send({error,data,field});
         })
         break; 
 
         case 'bank_payment':
-        pool.query('CALL DELETE_BANK_PAYMENT(?)',[req.body.id],(error,data,field)=>{
-            res.send({error,data:data?data[0]:null,field:field?field[0]:null});
+        pool.query('CALL DELETE_BANK_PAYMENT(?)',[req.body.id.map(item=>JSON.stringify(item)).join()],(error,data,field)=>{
+            res.send({error,data,field});
         })
         break; 
 
         case 'cash_receipt':
-        pool.query('CALL DELETE_CASH_RECEIPT(?)',[req.body.id],(error,data,field)=>{
-            res.send({error,data:data?data[0]:null,field:field?field[0]:null});
+        pool.query('CALL DELETE_CASH_RECEIPT(?)',[req.body.id.map(item=>JSON.stringify(item)).join()],(error,data,field)=>{
+            res.send({error,data,field});
         })
         break; 
 
         case 'cash_payment':
-        pool.query('CALL DELETE_CASH_PAYMENT(?)',[req.body.id],(error,data,field)=>{
-            res.send({error,data:data?data[0]:null,field:field?field[0]:null});
+        pool.query('CALL DELETE_CASH_PAYMENT(?)',[req.body.id.map(item=>JSON.stringify(item)).join()],(error,data,field)=>{
+            res.send({error,data,field});
         })
         break; 
 
         case 'journal':
-        pool.query('CALL DELETE_JOURNAL(?)',[req.body.id],(error,data,field)=>{
-            res.send({error,data:data?data[0]:null,field:field?field[0]:null});
+        pool.query('CALL DELETE_JOURNAL(?)',[req.body.id.map(item=>JSON.stringify(item)).join()],(error,data,field)=>{
+            res.send({error,data,field});
         })
         break; 
 
