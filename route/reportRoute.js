dafe0789e87f37d3bcd9d6956f26a7d40f32322b
mseong <mseong123@hotmail.com>
2020-04-07 16:08:27 +0800

    added new feature debtor collection report

--- route/reportRoute.js
+++ route/reportRoute.js
@@ -17,6 +17,16 @@ function reportRoute(req,res,next,pool) {
                 res.send({error,data:data?data:null,field:field?field:null});
             })
         break;
+
+        case 'debtor_collection':
+            pool.query('CALL SELECT_DEBTOR_COLLECTION(?,?,?)',[JSON.stringify(req.body.param[0])
+            ,JSON.stringify(req.body.param[1]),req.body.param[2].map(item=>JSON.stringify(item)).join()]
+            ,(error,data,field)=>{
+                dateParser(data?data[0]:null,field?field[0]:null)
+                dateParser(data?data[1]:null,field?field[1]:null)
+                res.send({error,data:data?data:null,field:field?field:null});
+            })
+        break;
     }
 }
 
